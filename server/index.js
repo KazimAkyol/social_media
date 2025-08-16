d6cc2472fd2563e37746a7744d8cbd07f798f198
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-16 17:10:12 +0200

    added auth.js-routes

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
-
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,7 +40,7 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
-
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
