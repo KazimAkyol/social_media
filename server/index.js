f81af996d651c6c3543414dc36f8a009ded066ec
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-17 15:44:08 +0200

    updated users.js-controllers

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
-app.use("/users, userRoutes");
+app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
