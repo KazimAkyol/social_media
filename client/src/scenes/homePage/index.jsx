ab645b6d6310650079aa4d137d3c89a10eb26fe5
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-17 17:21:22 +0200

    added index.jsx-homePage

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from 'react-redux';
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidgest";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+
+const HomePage = () => {
+
+    // 01:50:26
+  return (
+    <Box>
+
+    </Box>
+  )
+}
+
+export default HomePage
