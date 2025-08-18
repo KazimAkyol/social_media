0b70d4f10b446b01e3b4a8c605a7cef3599112a3
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-18 23:53:06 +0200

    updated index.js-loginPage

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
     return (
         <div>LoginPage</div>
     )
