fbb4f49bb9b2a7991e4bc19b0475110a6ff62f49
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-19 09:19:18 +0200

    updated index.js-loginPage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from 'react-redux';
 import Navbar from "scenes/navbar";
-import UserWidget from "scenes/widgets/UserWidget";
-import PostsWidget from "scenes/widgets/PostsWidget";
-import AdvertWidget from "scenes/widgets/AdvertWidgest";
-import FriendListWidget from "scenes/widgets/FriendListWidget";
+// import UserWidget from "scenes/widgets/UserWidget";
+// import PostsWidget from "scenes/widgets/PostsWidget";
+// import AdvertWidget from "scenes/widgets/AdvertWidgest";
+// import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-    const { _id, picturePath } = useSelector((state) => state.user);
-
+    // const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    // const { _id, picturePath } = useSelector((state) => state.user);
+    
     return (
         <Box>
-
+            <Navbar />
         </Box>
     )
 }
