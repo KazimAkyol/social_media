377cc91930cff4385600b03c17292d38c7000f1e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-20 17:46:25 +0200

    added MyPostWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -137,10 +137,23 @@ const UserWidget = ({ userId, picturePath }) => {
 
                 <FlexBetween gap="1rem" mb="0.5rem">
                     <FlexBetween gap="1rem">
-                        <img src="../assets.linkedin.png" alt="linkedin" />
+                        <img src="../assets/twitter.png" alt="twitter" />
                         <Box>
                             <Typography color={main} fontWeight="500">
-                                LinkedIn
+                                Twitter
+                            </Typography>
+                            <Typography color={medium}>Social Network</Typography>
+                        </Box>
+                    </FlexBetween>
+                    <EditOutlined sx={{ color: main }} />
+                </FlexBetween>
+
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="1rem">
+                        <img src="../assets/linkedin.png" alt="linkedin" />
+                        <Box>
+                            <Typography color={main} fontWeight="500">
+                                Linkedin
                             </Typography>
                             <Typography color={medium}>Network Platform</Typography>
                         </Box>
