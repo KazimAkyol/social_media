c7d25db26736cd937f4678588192c1198717f63a
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-21 13:38:50 +0200

    last changed MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -49,7 +49,7 @@ const MyPostWidget = ({ picturePath }) => {
         const response = await fetch(`http://localhost:3001/posts`,
             {
                 method: "POST",
-                headers: { Authorization: `Bearer $ {token}` },
+                headers: { Authorization: `Bearer ${token}` },
                 body: formData,
             });
         const posts = await response.json();
@@ -70,7 +70,7 @@ const MyPostWidget = ({ picturePath }) => {
                         width: "100%",
                         backgroundColor: palette.neutral.light,
                         borderRadius: "2rem",
-                        padding: "1rem, 2rem"
+                        padding: "1rem 2rem",
                     }}
                 />
             </FlexBetween>
@@ -122,15 +122,53 @@ const MyPostWidget = ({ picturePath }) => {
             <Divider sx={{ margin: "1.25rem 0" }} />
 
             <FlexBetween>
-                <FlexBetween
-                    gap="0.25rem"
-                    onClick={() => setIsImage(!isImage)}
-                >
+                <FlexBetween gap="0.25rem" onClick={() => setIsImage(!isImage)}>
                     <ImageOutlined sx={{ color: mediumMain }} />
+                    <Typography
+                        color={mediumMain}
+                        sx={{ "&:hover": { cursor: "pointer", color: medium } }}
+                    >
+                        Image
+                    </Typography>
                 </FlexBetween>
+
+                {isNonMobileScreens ? (
+                    <>
+                        <FlexBetween gap="0.25rem">
+                            <GifBoxOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Clip</Typography>
+                        </FlexBetween>
+
+                        <FlexBetween gap="0.25rem">
+                            <AttachFileOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Attachment</Typography>
+                        </FlexBetween>
+
+                        <FlexBetween gap="0.25rem">
+                            <MicOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Audio</Typography>
+                        </FlexBetween>
+                    </>
+                ) : (
+                    <FlexBetween gap="0.25rem">
+                        <MoreHorizOutlined sx={{ color: mediumMain }} />
+                    </FlexBetween>
+                )}
+
+                <Button
+                    disabled={!post}
+                    onClick={handlePost}
+                    sx={{
+                        color: palette.background.alt,
+                        backgroundColor: palette.primary.main,
+                        borderRadius: "3rem",
+                    }}
+                >
+                    POST
+                </Button>
             </FlexBetween>
         </WidgetWrapper>
-    )
+    );
 };
-
+ 
 export default MyPostWidget;
