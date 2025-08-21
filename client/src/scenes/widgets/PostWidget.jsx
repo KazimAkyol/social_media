5fe6eaa30286062d8b4d973bd732c4290814d3c4
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-21 14:37:54 +0200

    added Friends.jsx

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,15 @@
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBonderOutlined,
+    FavoriteOutlined,
+    ShareOutlined
+} from "@mui/icons-material";
+import {
+    Box,
+    Divider,
+    IconButton,
+    Typography,
+    useTheme
+} from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import 
